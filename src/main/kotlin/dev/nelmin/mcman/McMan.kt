d9f5ee9ea55070bbf4398e8fa08c44d2982ea181
Maximilian Guthmann <github.encourage540@passmail.net>
2024-12-15 18:55:20 +0100

    Initial commit

---
+++ src/main/kotlin/dev/nelmin/mcman/McMan.kt
@@ -0,0 +1,14 @@
+package dev.nelmin.mcman
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class McMan : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
